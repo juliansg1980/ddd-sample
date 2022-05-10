0f15b121265b3d0ce7c3c1ac6619683aec96bb99
Julian Sanz <juliansg1980@hotmail.com>
2022-05-10 14:12:13 +0200

    green has_empty_products

---
+++ ddd-sample-tests/domain/CartShould.cs
@@ -0,0 +1,18 @@
+﻿using ddd_sample_domain;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace ddd_sample_tests.domain
+{
+    [TestFixture]
+    internal class CartShould
+    {
+        [Test]
+        public void has_empty_products()
+        {
+            var cart = new Cart();
+
+            cart.Products.Should().BeEmpty();
+        }
+    }
+}
